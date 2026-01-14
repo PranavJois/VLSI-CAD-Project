75f1826d06abc72dde78ceae38f4beeb4899d68b
Pranav Jois <162716050+PranavJois@users.noreply.github.com>
2026-01-14 10:36:31 +0530

    Create gray2bin.v

---
+++ RTL/gray2bin.v
@@ -0,0 +1,11 @@
+module gray2bin #(parameter W=4)(
+    input  wire [W:0] gray,
+    output reg  [W:0] bin
+);
+    integer i;
+    always @(*) begin
+        bin[W] = gray[W];
+        for (i = W-1; i >= 0; i = i-1)
+            bin[i] = bin[i+1] ^ gray[i];
+    end
+endmodule

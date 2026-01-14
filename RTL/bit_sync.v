5e61050ce1e2348a0959855481d47ff9e0fb781e
Pranav Jois <162716050+PranavJois@users.noreply.github.com>
2026-01-14 10:35:33 +0530

    Update bit_sync.v

--- RTL/bit_sync.v
+++ RTL/bit_sync.v
@@ -1,1 +1,19 @@
+module bit_sync (
+    input  wire clk,
+    input  wire rst,
+    input  wire d,
+    output reg  q
+);
+    reg q1;
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            q1 <= 1'b0;
+            q  <= 1'b0;
+        end else begin
+            q1 <= d;
+            q  <= q1;
+        end
+    end
+endmodule
 

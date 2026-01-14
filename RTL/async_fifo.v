9831d70a7699c8e693551e31c015dca74a4231f2
Pranav Jois <162716050+PranavJois@users.noreply.github.com>
2026-01-14 12:48:55 +0530

    Update async_fifo.v

--- RTL/async_fifo.v
+++ RTL/async_fifo.v
@@ -1,16 +1,16 @@
 module async_fifo #(
-    parameter DATA_W = 32,
-    parameter ADDR_W = 3   // depth = 8
+    parameter DATA_W = 65,
+    parameter ADDR_W = 3
 )(
     input  wire                 wr_clk,
     input  wire                 rd_clk,
     input  wire                 rst,
 
-    input  wire                 wr_en,
+    input  wire                 wr_fire,
     input  wire [DATA_W-1:0]    wr_data,
     output wire                 full,
 
-    input  wire                 rd_en,
+    input  wire                 rd_fire,
     output reg  [DATA_W-1:0]    rd_data,
     output wire                 empty
 );
@@ -19,18 +19,14 @@ module async_fifo #(
 
     reg [DATA_W-1:0] mem [0:DEPTH-1];
 
-    reg [ADDR_W:0] wptr_bin, rptr_bin;
+    reg  [ADDR_W:0] wptr_bin, rptr_bin;
     wire [ADDR_W:0] wptr_gray, rptr_gray;
 
     reg [ADDR_W:0] wptr_gray_rdclk, rptr_gray_wrclk;
 
-    // Sync pointers
-    bit_sync sync_wptr0(.clk(rd_clk), .rst(rst), .d(wptr_gray[0]), .q(wptr_gray_rdclk[0]));
-    bit_sync sync_rptr0(.clk(wr_clk), .rst(rst), .d(rptr_gray[0]), .q(rptr_gray_wrclk[0]));
-
     genvar i;
     generate
-        for (i=1; i<=ADDR_W; i=i+1) begin
+        for (i=0; i<=ADDR_W; i=i+1) begin
             bit_sync sw(.clk(rd_clk), .rst(rst), .d(wptr_gray[i]), .q(wptr_gray_rdclk[i]));
             bit_sync sr(.clk(wr_clk), .rst(rst), .d(rptr_gray[i]), .q(rptr_gray_wrclk[i]));
         end
@@ -39,29 +35,30 @@ module async_fifo #(
     bin2gray #(ADDR_W) b2g_w (.bin(wptr_bin), .gray(wptr_gray));
     bin2gray #(ADDR_W) b2g_r (.bin(rptr_bin), .gray(rptr_gray));
 
-    // Write logic
     always @(posedge wr_clk or posedge rst) begin
-        if (rst) begin
+        if (rst)
             wptr_bin <= 0;
-        end else if (wr_en && !full) begin
+        else if (wr_fire && !full) begin
             mem[wptr_bin[ADDR_W-1:0]] <= wr_data;
             wptr_bin <= wptr_bin + 1;
         end
     end
 
-    // Read logic
     always @(posedge rd_clk or posedge rst) begin
         if (rst) begin
             rptr_bin <= 0;
             rd_data  <= 0;
-        end else if (rd_en && !empty) begin
+        end else if (rd_fire && !empty) begin
             rd_data  <= mem[rptr_bin[ADDR_W-1:0]];
             rptr_bin <= rptr_bin + 1;
         end
     end
 
-    // Empty / Full
     assign empty = (rptr_gray == wptr_gray_rdclk);
-    assign full  = ( (wptr_gray == {~rptr_gray_wrclk[ADDR_W:ADDR_W-1], rptr_gray_wrclk[ADDR_W-2:0]}) );
+
+    assign full  = (wptr_gray == {
+                    ~rptr_gray_wrclk[ADDR_W:ADDR_W-1],
+                     rptr_gray_wrclk[ADDR_W-2:0]
+                   });
 
 endmodule

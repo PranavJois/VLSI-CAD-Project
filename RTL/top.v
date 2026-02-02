0f31854207016d409e0a6d4fbffeb59af3fd6c38
Pranav Jois <162716050+PranavJois@users.noreply.github.com>
2026-02-02 22:31:49 +0530

    Update top.v

--- RTL/top.v
+++ RTL/top.v
@@ -1,14 +1,17 @@
-
-module top(input clk_a, clk_b, rst);
+module top(
+    input clk_a,
+    input rst,
+    output [31:0] result,
+    output done
+);
 
 wire [31:0] paddr_a, pwdata_a, prdata_a;
 wire pwrite_a, psel_a, penable_a, pready_a;
-
+wire clk_b;
 wire [31:0] paddr_b, pwdata_b, prdata_b;
 wire pwrite_b, psel_b, penable_b, pready_b;
 
-wire [31:0] result;
-wire done;
+clk_divider divider(clk_a, clk_b);
 
 apb_master M(clk_a, rst, paddr_a, pwdata_a, pwrite_a, psel_a, penable_a,
              pready_a, prdata_a, result, done);
@@ -23,4 +26,3 @@ apb_slave S(clk_b, rst, paddr_b, pwdata_b, pwrite_b, psel_b, penable_b,
             pready_b, prdata_b);
 
 endmodule
-
